b308949f239955965f10bfa1b84db4f4657afed2
s2fujita <s2fujita@oec.co.jp>
2022-06-08 17:22:34 +0900

    e2eテスト

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # sandbox
+
+## PlayWright x GitHub Actions
+
+<https://zenn.dev/keita_hino/articles/d38956a2f1880e>
---
+++ src/tests/countUp.spec.ts
@@ -0,0 +1,15 @@
+import { test, expect } from "@playwright/test";
+
+// 各テストを実行する前に http://localhost:3000 に遷移する
+test.beforeEach(async ({ page }) => {
+  await page.goto("http://localhost:3000");
+});
+
+test("カウントアップボタンをクリックすると、カウントが増加すること", async ({
+  page,
+}) => {
+  // data-testid 属性が count-up-button な要素をクリックする
+  await page.locator("data-testid=count-up-button").click();
+  // data-testid 属性が count な要素のテキストが1になっていることを確認
+  await expect(page.locator("data-testid=count")).toContainText("1");
+});
