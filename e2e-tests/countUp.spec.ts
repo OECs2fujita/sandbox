e696bf14e1d5830ed1afeab389a377d07275f38e
s2fujita <s2fujita@oec.co.jp>
2022-06-10 17:39:00 +0900

    テスト内容の変更

--- e2e-tests/countUp.spec.ts
+++ e2e-tests/countUp.spec.ts
@@ -9,9 +9,14 @@ test.beforeEach(async ({ page }) => {
 
 test("カウントアップボタンをクリックすると、カウントが増加すること", async ({
   page,
-}) => {
+}, testInfo) => {
   // data-testid 属性が count-up-button な要素をクリックする
   await page.locator("data-testid=count-up-button").click();
   // data-testid 属性が count な要素のテキストが1になっていることを確認
   await expect(page.locator("data-testid=count")).toContainText("1");
+
+  await page.screenshot({
+    path: testInfo.outputPath("screenshot/1_main.png"),
+    fullPage: true,
+  });
 });
